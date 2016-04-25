165e006f7712e180d552a531736eb160887163cf
Nico Güttler <nico.guettler@finalfrontierlabs.com>
2016-04-25 18:22:25 +0200

    Added jenkins embeded.

---
+++ appengine-test/test/testMainPage.py
@@ -0,0 +1,27 @@
+'''
+Created on Apr 25, 2016
+
+@author: user
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+    def testTwo(self):
+        self.assertEqual(4, 2+2, None)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ appengine-test/test/testQuickSort.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 25, 2016
+
+@author: user
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
